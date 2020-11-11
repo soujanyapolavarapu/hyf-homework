60f077e1715aca24b1af9f71901fc5bab9d5e7f8
soujanyapolavarapu <soujanyabonthu@gmail.com>
2020-11-12 02:48:16 +0530

    added Item-array-removal file to repo

---
+++ javascript/javascript1/week3/Item-array-removal.js
@@ -0,0 +1,16 @@
+const names = [
+  "Peter",
+  "Ahmad",
+  "Yana",
+  "kristina",
+  "Rasmus",
+  "Samuel",
+  "katrine",
+  "Tala",
+];
+
+const nameToRemove = "Ahmad";
+
+names.splice(1,1);
+
+console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
