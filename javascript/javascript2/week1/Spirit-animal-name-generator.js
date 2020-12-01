a9203c70dfffc062c59a9f13c3c1432558a19e01
soujanyapolavarapu <soujanyabonthu@gmail.com>
2020-12-02 00:19:31 +0100

    modified Spirit-animal-name-generator.js

--- javascript/javascript2/week1/Spirit-animal-name-generator.js
+++ javascript/javascript2/week1/Spirit-animal-name-generator.js
@@ -1,42 +1,48 @@
+const click = 'click';
+const newsprit = 'newsprit';
+let enterInNewsprit = 'no'
 const accessButton = document.getElementById("button");
 const button_two = document.createElement("button");
 button.setAttribute("id", "buttonTwo");
-button_two.innerHTML ="NewSprit";
+button_two.innerHTML = "NewSprit";
 document.body.appendChild(button_two);
-button_two.style.display= "none";
+button_two.style.display = "none";
 
-const spritAnimalName =['wolf','Alligator','Badger', 'Bee','Bobcat', 'Blue Jay','ant','butterfly','owl','snake'];
-let randomNumber = Math.floor(Math.random() * 10) ;
+const spritAnimalName = ['wolf', 'Alligator', 'Badger', 'Bee', 'Bobcat', 'Blue Jay', 'ant', 'butterfly', 'owl', 'snake'];
+let randomNumber = Math.floor(Math.random() * 10);
+let randomNumber1;
+
 
 const spritAnimal =
-function() {
-let randomSpritAnimal;
-     const accessInput = document.getElementById("user_input").value;
-     h1_tag =  document.getElementById('h1')
-     if(accessInput === ""){
-       h1_tag.innerHTML="Please enter your Name";
-     }else{
-     randomSpritAnimal = spritAnimalName[randomNumber];
-h1_tag.innerHTML= `${accessInput} - ${randomSpritAnimal}` ;
-button_two.style.display= "block";
-button_two.style.disabled= true;
- }
-}
+  function spritAnimal(spiritOrNewSpirit) {
+    let randomSpritAnimal;
+    let accessInput = document.getElementById("user_input").value;
+    h1_tag = document.getElementById('h1')
+    if (accessInput === "") {
+      h1_tag.innerHTML = "Please enter your Name";
+    } else {
+      if (spiritOrNewSpirit === 'newsprit') {
+        randomNumber1 = Math.floor(Math.random() * 10);
+        randomSpritAnimal = spritAnimalName[randomNumber1];
+        enterInNewsprit = 'yes'
+      } else {
+        if (enterInNewsprit === 'no') {
+          randomSpritAnimal = spritAnimalName[randomNumber];
+        } else {
+          randomSpritAnimal = spritAnimalName[randomNumber1];
+        }
 
-   accessButton.addEventListener('click', spritAnimal);
-  // button_two.addEventListener('click', spritAnimal);
+      }
+      h1_tag.innerHTML = `${accessInput} - ${randomSpritAnimal}`;
+      button_two.style.display = "block";
+      button_two.style.disabled = true;
 
- const NewspritAnimal =
- function() {
-   let randomNumber2 = Math.floor(Math.random() * 10) ;
-   const accessInput = document.getElementById("user_input").value;
-   h1_tag =  document.getElementById('h1')
+    }
 
-   if(accessInput === ""){
-     h1_tag.innerHTML="Please enter your Name";
-   }else{
-  let  randomSpritAnimal = spritAnimalName[randomNumber2];
- h1_tag.innerHTML= `${accessInput} - ${randomSpritAnimal}` ;
-   }
- }
- button_two.addEventListener('click', NewspritAnimal);
+  }
+accessButton.addEventListener('click', function() {
+  spritAnimal(click)
+});
+button_two.addEventListener('click', function() {
+  spritAnimal(newsprit)
+});
