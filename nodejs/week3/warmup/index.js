bb4301d87c91b9d6bd46930aff6e482da384cab5
soujanya polavarapu <soujanyabonthu@gmail.com>
2021-02-24 00:39:23 +0100

    updated index/warmup file

--- nodejs/week3/warmup/index.js
+++ nodejs/week3/warmup/index.js
@@ -20,7 +20,7 @@ router.get("/multiply", (request, response) => {
 router.get("/add", (request, response) => {
     try {
       const values = Object.values(request.query).flat();
-     const addValues =values.reduce((accumulator,currentValue)=> parseInt(accumulator + currentValue));
+     const addValues =values.reduce((accumulator,currentValue)=> parseInt(+accumulator + +currentValue));
       response.send(` Your result is ${addValues}`);
     } catch (error) {
       throw error;
@@ -28,6 +28,7 @@ router.get("/add", (request, response) => {
   }); 
 
   router.get("/subtraction" , (request,response)=>{
+
     try{
         const values = Object.values(request.query).flat();
        const subValues =values.reduce((accumulator,currentValue)=> parseInt(accumulator - parseInt(currentValue)));
@@ -39,30 +40,30 @@ router.get("/add", (request, response) => {
   });
 //{}
 
-router.post("/multiply" , (request,response)=>{
-  try{
+// router.post("/multiply" , (request,response)=>{
+//   try{
     
-      let numOne = Number(request.body.firstParam);
-      let numTwo = Number(request.body.secondParam);
-      const multiplication = numOne * numTwo;
-      response.send(` Your result is ${multiplication}`);
-  }
-  catch(error){
-    throw(error);
-  }
+//       let numOne = Number(request.body.firstParam);
+//       let numTwo = Number(request.body.secondParam);
+//       const multiplication = numOne * numTwo;
+//       response.send(` Your result is ${multiplication}`);
+//   }
+//   catch(error){
+//     throw(error);
+//   }
 
-});
+// });
 
-router.post("/division" , (request,response)=>{
-  try{
-      const values = Object.values(request.body).flat();
-     const subValues =values.reduce((accumulator,currentValue)=> parseInt(accumulator / parseInt(currentValue)));
-      response.send(` Your result is ${subValues}`);
-  }
-  catch(error){
-    throw(error);
-  }
-});
+// router.post("/division" , (request,response)=>{
+//   try{
+//       const values = Object.values(request.body).flat();
+//      const subValues =values.reduce((accumulator,currentValue)=> parseInt(accumulator / parseInt(currentValue)));
+//       response.send(` Your result is ${subValues}`);
+//   }
+//   catch(error){
+//     throw(error);
+//   }
+// });
 
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
 
