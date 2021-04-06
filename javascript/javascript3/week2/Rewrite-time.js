2fb15f114135033aa55f4f4b77d675d58660b831
soujanya polavarapu <soujanyabonthu@gmail.com>
2021-04-06 22:35:19 +0200

    modified files according to mentors comments

--- javascript/javascript3/week2/Rewrite-time.js
+++ javascript/javascript3/week2/Rewrite-time.js
@@ -1,40 +1,20 @@
-
-/*function setTimeoutPromise(givenTime){
-  console.log('I was called immediately');
-  const promise = new Promise((resolve,reject)=>{
-    //const settingTime = givenTime;
-    setTimeout(()=>{
-      resolve(givenTime);
-    },givenTime)
+function getCurrentLocation() {
+  const geolocationPromise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => resolve(position),
+      (error) => reject(error)
+    );
   });
-  return promise;
+  return geolocationPromise;
 }
-
-
-setTimeoutPromise(9000)
-.then((givenTime) => {
-  console.log(`I was loggedOut after ${givenTime}`);
-})
-.catch((err)=>{
-  console.log('error');
-});*/
-
-
-function getCurrentLocation(){
-  const geolocationPromise = new Promise((resolve,reject)=>{
-  navigator.geolocation.getCurrentPosition((position)=>resolve(position), (error)=>reject(error));
-});
-return geolocationPromise();
-}
-
 
 getCurrentLocation()
   .then((position) => {
-    const latitude  = position.coords.latitude;
+    const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     console.log(`Latitude: ${latitude} °, Longitude: ${longitude} °`);
   })
   .catch((error) => {
     // called if there was an error getting the users location
-    console.log('error');
+    console.log("error");
   });
--- javascript/javascript3/week2/main.js
+++ javascript/javascript3/week2/main.js
@@ -1,11 +1,14 @@
 // This code will move the li to the position 100, 100. Calling moveElement will return a promise that resolves after the li element has been moved.
+const redBox  = document.querySelector("ul.marks li:nth-child(1)");
+const blueBox = document.querySelector("ul.marks li:nth-child(2)");
+const greenBox = document.querySelector("ul.marks li:nth-child(3)");
 
 function translateOneByOne(){
-    moveElement(document.querySelector("ul.marks li:nth-child(1)"), { x: 20, y: 300 }).then(() => {
+    moveElement(redBox, { x: 20, y: 300 }).then(() => {
         console.log("Red element has been moved");
-        moveElement(document.querySelector("ul.marks li:nth-child(2)"), { x: 400, y: 300 }).then(() => {
+        moveElement(blueBox, { x: 400, y: 300 }).then(() => {
             console.log("Blue element has been moved");
-            moveElement(document.querySelector("ul.marks li:nth-child(3)"), { x: 400, y: 20 }).then(() => {
+            moveElement(greenBox, { x: 400, y: 20 }).then(() => {
                 console.log("Green element has been moved");
             }).catch((err)=>console.log('Green element went wrong'))
         }).catch((err)=>console.log('Blue element went wrong'))
@@ -16,9 +19,9 @@ function translateOneByOne(){
 translateOneByOne();
 
 function translateAllAtOnce(){
-  Promise.all([moveElement(document.querySelector("ul.marks li:nth-child(1)"), { x: 20, y: 300 }),
-               moveElement(document.querySelector("ul.marks li:nth-child(2)"), { x: 400, y: 300 }),
-               moveElement(document.querySelector("ul.marks li:nth-child(3)"), { x: 400, y: 20 })]).then((values) => {
+  Promise.all([moveElement(redBox, { x: 20, y: 300 }),
+               moveElement(blueBox, { x: 400, y: 300 }),
+               moveElement(greenBox, { x: 400, y: 20 })]).then((values) => {
    console.log('All elements has been moved');
  });
 }
